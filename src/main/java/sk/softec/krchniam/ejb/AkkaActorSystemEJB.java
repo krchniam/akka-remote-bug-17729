b9c2cdc873ca8cffd68d14d40b49acf867702056
Matej Krchniak <matej.krchniak@softec.sk>
2015-06-23 17:47:03 +0200

    Removed unnecessary files

--- src/main/java/sk/softec/krchniam/ejb/AkkaActorSystemEJB.java
+++ src/main/java/sk/softec/krchniam/ejb/AkkaActorSystemEJB.java
@@ -1,55 +1,30 @@
 package sk.softec.krchniam.ejb;
 
-import java.util.concurrent.AbstractExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.annotation.Resource;
-import javax.ejb.ConcurrencyManagement;
-import javax.ejb.ConcurrencyManagementType;
-import javax.ejb.Local;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-
+import akka.actor.ActorSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sk.softec.krchniam.ejb.local.ActorSystemService;
-import sk.softec.krchniam.ejb.local.WrappedException;
-
-import akka.actor.ActorSelection;
-import akka.actor.ActorSystem;
-import akka.pattern.Patterns;
-import scala.concurrent.Await;
-import scala.concurrent.ExecutionContext;
-import scala.concurrent.Future;
-import scala.concurrent.duration.FiniteDuration;
-import scala.concurrent.impl.ExecutionContextImpl;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 @Singleton
 @Startup
-@Local(ActorSystemService.class)
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
-public class AkkaActorSystemEJB implements ActorSystemService {
+public class AkkaActorSystemEJB {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AkkaActorSystemEJB.class);
 
 	private ActorSystem actorSystem;
 
-	private FiniteDuration askDurationTimeout;
-
-	@Resource(lookup = "iszi/akka-remote-system")
-	private String akkaRemote;
-
-	@Resource(lookup = "iszi/akka-ask-timeout")
-	private Integer askDurationTimeoutInteger;
-
 	@PostConstruct
 	public void init() {
-		askDurationTimeout = FiniteDuration.create(askDurationTimeoutInteger, TimeUnit.SECONDS);
 		actorSystem = ActorSystem.create("akka-integration");
 		// shutdown uses scala.concurrent.ExecutionContext.Implicits.global, adding hook for shutting down
+		//
 //		actorSystem.registerOnTermination(new Runnable() {
 //			@Override
 //			public void run() {
@@ -74,38 +49,5 @@ public class AkkaActorSystemEJB implements ActorSystemService {
 		actorSystem.shutdown();
 		actorSystem.awaitTermination();
 		LOGGER.info("ActorSystem terminated.");
-	}
-
-	@Override
-	public <T> T ask(String actorName, Object message) throws WrappedException {
-		String path = createRemotePath(actorName);
-		try {
-			return askSynchronous(message, path);
-		} catch (Exception e) {
-			throw new WrappedException( String.format("Calling of remote actor on address: %s failed", path), e);
-		}
-	}
-
-	private String createRemotePath(String actorName) {
-		String path = String.format("%s/user/%s", akkaRemote, actorName);
-		LOGGER.debug("remotePath created: {}", path);
-		return path;
-	}
-
-	@SuppressWarnings("unchecked")
-	private <T> T askSynchronous(Object message, String path) throws Exception {
-		return (T)Await.result(askActor(path, message), askDurationTimeout);
-	}
-
-	private Future<Object> askActor(String path, Object message) {
-		ActorSelection actorSelection = actorSystem.actorSelection(path);
-		return Patterns.ask(actorSelection, message, askDurationTimeout.length());
-	}
-
-	@Override
-	public void tell(String actorName, Object message) {
-		String path = createRemotePath(actorName);
-		ActorSelection actorSelection = actorSystem.actorSelection(path);
-		actorSelection.tell(message, null);
 	}
 }
--- src/main/java/sk/softec/krchniam/ejb/local/ActorSystemService.java
+++
@@ -1,7 +0,0 @@
-package sk.softec.krchniam.ejb.local;
-
-public interface ActorSystemService {
-	<T> T ask(String actorName, Object message) throws WrappedException;
-
-	void tell(String actorName, Object message);
-}
